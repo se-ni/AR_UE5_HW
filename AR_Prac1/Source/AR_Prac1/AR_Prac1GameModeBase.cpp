a2a90da9f6e5d3a88ce150ea7507357c6018c715
Part SeEun <20000109s@gmail.com>
2023-06-18 21:15:05 +0900

    initial commit

---
+++ AR_Prac1/Source/AR_Prac1/AR_Prac1GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "AR_Prac1GameModeBase.h"
+
---
+++ AR_Prac1/Source/AR_Prac1/AR_Prac1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AR_Prac1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AR_PRAC1_API AAR_Prac1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
