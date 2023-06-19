44cf491b0c4c706234c5d8236a0291d634887383
Part SeEun <20000109s@gmail.com>
2023-06-19 18:21:33 +0900

    6/19 commit

---
+++ Snake_Prac/Source/Snake_Prac/Snake_Prac.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Snake_Prac.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Snake_Prac, "Snake_Prac" );
---
+++ Snake_Prac/Source/Snake_Prac/Snake_PracGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Snake_PracGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNAKE_PRAC_API ASnake_PracGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
