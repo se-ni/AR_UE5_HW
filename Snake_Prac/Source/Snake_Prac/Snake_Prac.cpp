9c84141c7c02a2f7d354fd89291522cb1649d8ed
Park SeEun <101172040+se-ni@users.noreply.github.com>
2023-06-22 16:14:17 +0900

    Delete Snake_Prac directory

--- Snake_Prac/Source/Snake_Prac/Snake_Prac.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Snake_Prac.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Snake_Prac, "Snake_Prac" );
--- Snake_Prac/Source/Snake_Prac/Snake_PracGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "Snake_PracGameModeBase.h"
-
